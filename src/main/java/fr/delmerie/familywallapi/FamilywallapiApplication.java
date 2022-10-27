ac2f1fae298246a8b57eaddbd44b72c15ecd2027
Delmerie <j.delmerie@live.fr>
2022-10-27 10:16:51 +0200

    Init back : test ok

---
+++ src/main/java/fr/delmerie/familywallapi/FamilywallapiApplication.java
@@ -0,0 +1,18 @@
+package fr.delmerie.familywallapi;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilywallapiApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilywallapiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
---
+++ src/test/java/fr/delmerie/familywallapi/FamilywallapiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.delmerie.familywallapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilywallapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
